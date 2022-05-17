90d927bdb53aa4168b9b4e3cd61c5633671fb893
Justyna Dudar <justyna.dudar1@gmail.com>
2022-05-17 12:43:39 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { RemoveUserComponent } from './remove-user/remove-user.component';
+import { NavComponent } from './nav/nav.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: UsersListComponent,
+  },
+  {
+    path: 'add',
+    component: AddUserComponent,
+  },
+  {
+    path: 'remove',
+    component: RemoveUserComponent,
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    AddUserComponent,
+    RemoveUserComponent,
+    NavComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
